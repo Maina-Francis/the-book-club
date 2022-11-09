6f780021a138a7e57037487de011cb00673db8ae
Francis Maina <sebastianfrancix@gmail.com>
2022-11-09 21:58:03 +0300

    added responsiveness to the hamburger

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import "../navbar.css";
 
 function Navbar() {
+  // hamburger active state
+  const [active, setActive] = useState("nav_menu");
+
+  const navToggle = () => {
+    active === "nav_menu"
+      ? setActive("nav_menu nav_active")
+      : setActive("nav_menu");
+  };
+
   return (
     <nav className="nav">
       <a href="#" className="logo">
@@ -9,7 +18,7 @@ function Navbar() {
       </a>
 
       {/* Navbar menu  */}
-      <ul className="nav_menu">
+      <ul className={active}>
         <li className="nav_item">
           <a href="#" className="nav_link">
             Library
@@ -28,7 +37,7 @@ function Navbar() {
       </ul>
 
       {/* Hamburger Icon  */}
-      <div className="hamburger">
+      <div onClick={navToggle} className="hamburger">
         <div className="line1"></div>
         <div className="line2"></div>
         <div className="line3"></div>
