9890fb65eb559dc41ff8df32683deb47b8533b5a
Calvin <ncalvin67@gmail.com>
2022-11-09 18:14:00 +0300

    Added card component and its functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/js/bootstrap.js';
+import Library from "./Components/Library";
 
 function App() {
-  return <div>App</div>;
+  return (
+  <>
+  <Library />
+  </>
+  );
 }
 
 export default App;
---
+++ src/Components/Card.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+const Card = ({book}) => {
+  return (
+    <>
+      <div className="col">
+        <div className="card h-100">
+          <img
+            src={book.image}
+            className="card-img-top"
+            alt="..."
+            height={330}
+          />
+          <div className="card-body">
+            <h4 className="card-title">{book.title}</h4>
+            <p className="card-text">{book.author}</p>
+            <small className="text-muted">{book.genre}</small>
+            <p className="card-text">{book.publisher}</p>
+            <p className="card-text text-success">{book.rating}⭐⭐⭐⭐</p>
+            <div className="card-button row d-flex justify-content-between">
+              <button
+                onClick={()=>{}}
+                className="btn btn-primary ms-2 mb-1"
+                style={{ width: "120px" }}
+              >
+                Add to Cart
+              </button>
+              <button
+                onClick={()=>{}}
+                className="btn btn-danger ms-2 mb-1"
+                style={{ width: "120px" }}
+              >
+                Delete
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Card
