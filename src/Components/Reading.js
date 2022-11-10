d8cdacc2c9e1b836ec7d624c25e0aa0ff3eb743d
Calvin <ncalvin67@gmail.com>
2022-11-10 18:24:49 +0300

    Added reading component

---
+++ src/Components/Reading.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Reading = () => {
+    const [books, setBooks] = useState([])
+  return (
+    <div>Reading</div>
+  )
+}
+
+export default Reading
--- src/index.js
+++ src/index.js
@@ -3,10 +3,9 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
+  <React.StrictMode>
     <App />
-  </Router>
+  </React.StrictMode>
 );
-
