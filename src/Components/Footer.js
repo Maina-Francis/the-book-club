8291d449d487e0b6cec83db1c08469dd7ddfe6dd
Patrobas Bwire <patrobasbwire@gmail.com>
2022-11-10 22:05:28 +0300

    Made color change to the Footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,9 +1,10 @@
 import React from "react";
 
+
 const Footer =()=>{
 
     return (
-        <footer className="py-5 bg-secondary">
+        <footer className="py-5 bg-dark">
             <div className="container"><p className="m-0 text-center text-white">Copyright &copy; All Rights Reserved</p></div>
         </footer>
     )
